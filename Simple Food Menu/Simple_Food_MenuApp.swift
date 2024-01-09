7958901a213da5f54528c5c32e8fed9d2e61f3b7
Jev Smolnikov <jev@iriepixel.com>
2024-01-09 21:44:53 +0000

    Initial Commit

---
+++ Simple Food Menu/Simple_Food_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Food_MenuApp.swift
+//  Simple Food Menu
+//
+//  Created by Jev Smolnikov on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Food_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
